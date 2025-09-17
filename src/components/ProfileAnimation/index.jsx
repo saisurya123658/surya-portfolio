21458b39a5ba00514b0c3b81532e3cc8b509bc21
A.SAI GURU SURYA TEJA <154744703+saisurya123658@users.noreply.github.com>
2025-09-17 08:51:12 +0530

    Update index.jsx

--- src/components/ProfileAnimation/index.jsx
+++ src/components/ProfileAnimation/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Div } from './profileanimationstyle';
-const HeroBgAnimation = () => ( 
+const HeroBgAnimation = () => (  
   <Div>
     <svg
       className="BgAnimation__svg"
