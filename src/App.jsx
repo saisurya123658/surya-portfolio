004c5031f69e6a25ce84fbd59e1ea2fb758aa5e3
AREVETI SAI GURU SURYA TEJA <154744703+saisurya123658@users.noreply.github.com>
2025-09-28 13:27:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Contact from "./components/section/Contact";
 import Footer from "./components/section/Footer";
 import CodingProfilesTimeline from "./components/section/codingprofiles";
 
-
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
